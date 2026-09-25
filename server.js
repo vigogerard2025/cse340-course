419ee119fc76def16161b45f2c76531cc1ccdfee
vigogerard2025 <vigogerard2025@0gmail.com>
2026-09-24 20:30:12 -0500

    categories.ejs

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { fileURLToPath } from "url";
 import path from "path";
-
+import { testConnection } from "./src/models/db.js";
+import { getAllOrganizations } from "./src/models/organizations.js";
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || "production";
 
@@ -37,6 +38,9 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/organizations", async (req, res) => {
+  const organizations = await getAllOrganizations();
+  console.log(organizations);
+
   const title = "Our Partner Organizations";
   res.render("organizations", { title });
 });
@@ -45,8 +49,16 @@ app.get("/projects", async (req, res) => {
   const title = "Service Projects";
   res.render("projects", { title });
 });
-
-app.listen(PORT, () => {
-  console.log(`Server is running at http://127.0.0.1:${PORT}`);
-  console.log(`Environment: ${NODE_ENV}`);
+app.get("/categories", async (req, res) => {
+  const title = "Service Project Categories";
+  res.render("categories", { title });
+});
+app.listen(PORT, async () => {
+  try {
+    await testConnection();
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+  }
 });
